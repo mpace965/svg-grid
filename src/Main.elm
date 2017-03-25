9f6c20fb371fb4e5ccb7df96314299f4251cc388
Matt Pace <pace4@purdue.edu>
2017-03-25 11:31:29 -0400

    Representation in ASCII

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
 import Array2D exposing (..)
+import Array exposing (Array)
 import Html exposing (..)
 
 
@@ -20,7 +21,8 @@ type alias Board =
 
 model : Board
 model =
-    repeat 10 10 Floor
+    (repeat 10 10 Floor)
+        |> set 0 0 Wall
 
 
 type Msg
@@ -36,4 +38,46 @@ update msg board =
 
 view : Board -> Html Msg
 view board =
-    div [] []
+    div [] (renderBoard board)
+
+
+cellText : Cell -> String
+cellText cell =
+    case cell of
+        Floor ->
+            "0"
+
+        Wall ->
+            "1"
+
+
+rowToDiv : Array String -> Html Msg
+rowToDiv row =
+    let
+        toSpan string =
+            span [] [ text string ]
+    in
+        div [] (Array.toList (Array.map toSpan row))
+
+
+textBoardToDivList : Array2D String -> List (Html Msg)
+textBoardToDivList textBoard =
+    let
+        buildRest textBoard divList =
+            case (getRow 0 textBoard) of
+                Just row ->
+                    buildRest (deleteRow 0 textBoard) (divList ++ [ (rowToDiv row) ])
+
+                Nothing ->
+                    divList
+    in
+        buildRest textBoard []
+
+
+renderBoard : Board -> List (Html Msg)
+renderBoard board =
+    let
+        textBoard =
+            Array2D.map cellText board
+    in
+        textBoardToDivList textBoard
